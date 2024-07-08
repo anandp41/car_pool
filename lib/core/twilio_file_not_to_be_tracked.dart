12a63ace7f52ad6ff88f15fd8ff120f5c959128b
Anand P <apanandp41@gmail.com>
2024-07-08 08:23:12 +0530

    Commit without twilio file

--- lib/core/twilio_file_not_to_be_tracked.dart
+++
@@ -1,4 +0,0 @@
-const serviceSID = 'VA79907a261223df734ff30e402c4bf67f';
-const authToken = 'f52e3116f19b3f75df56bbb1ad146dd4';
-const accountSID = 'AC1044c40bd75b68c4aa1603aef44cac3c';
-const twilioNumber = '+18576782029';
